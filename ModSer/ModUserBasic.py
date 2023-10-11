ec114f780b47ae2f77d010efe73698afb568f6ee
Bapt-Tech <baptisteh@mailo.com>
2023-10-11 17:02:50 +0200

    Ajout fonctionnalités ModUSerBasic

--- ModSer/ModUserBasic.py
+++ ModSer/ModUserBasic.py
@@ -9,12 +9,54 @@ def UserBasic():
         import time
         from ModSer import OsID
         from ModSer import ModLog
+
+        NIPCode = 1934
+
+        def WiFiServer():
+            while True :
+                print("Un code PIN est necessaire pour démarrer le serveur.")
+                NIPCodeInput = int(input("Entrez le code PIN : ")
+                if NIPCodeInput == NIPCode:
+                    print("Chargement...")
+                    from ModSer import HubServer
+                    HubServer.HubServer()
+                    break
+                else:
+                    print("Code PIN incorrect.")
+                    print("Réessayez SVP")
+        
+        def Games():
+            while True :
+                print("Un code PIN est necessaire pour Jouer. Entrez 0000 pour quitter.")
+                NIPCodeInput = int(input("Entrez le code PIN : ")
+                if NIPCodeInput == NIPCode:
+                    print("Chargement...")
+                    from ModSer import HubGames
+                    HubGames.HubGames()
+                    break
+                elif NIPCodeInput == (0000):
+                    break
+                else:
+                    print("Code PIN incorrect.")
+                    print("Réessayez SVP")
+
+
         print("Bienvenue sur ce nouveau service.")
         print("Chargement en cours...")
 
         while True:
             print("Menu")
             print("")
-            print("1. ")
+            print("1. Serveur WiFi")
+            print("2. Jeux")
+            print("3. Autres Apps")
+
+            Choix = int(input("Entrez votre choix : "))
+
+            if Choix == 1:
+                WiFiServer()
+            if Choix == 2:
+                Games()
+
     except:
         print("err")
