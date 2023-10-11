4319e8cb1c0afb2fb1938ae09039daee30031821
Bapt-Tech <137829833+Bapt-Tech@users.noreply.github.com>
2023-10-11 13:13:46 +0200

    Ajout de la différenciation des statuts

--- ModSer/ModCallSer.py
+++ ModSer/ModCallSer.py
@@ -24,103 +24,105 @@ def Services():
     ModLog.Log("--ModCallSer--\n")
     
     ModLog.Log("Connexion au LoginService pour Auth.\n")
+    Status = ModLogSer.LoginService()
 
-    a = ModLogSer.LoginService()
-
-    
-
-    print(a)
+    print("Votre statut à été défini sur : ", Status, " ! ")
     
     ModLog.Log("Ready\n")
+
+    if Status == ("Admin"):
     
-    while True:
+        while True:
+        
+            ModLog.Log("While True pour ModCallSer")
+            
+            #os.system(Term_clear) 
     
-        ModLog.Log("While True pour ModCallSer")
-        
-        #os.system(Term_clear) 
-
-        print("Tableau Administrateur")
-
-        print("Menu des services")
-        print()
-        print("1. Login Service.")
-        print("2. Jeux.")
-        print("3. Serveurs.")
-        print("4. Désactivation des services.")
-        print("5. Autres Apps")
-        print("6. Nouveautés")
-        print("7. Exit")
-
-
-        Choix = int(input("Entrez le service à appeler : "))
-
-        if Choix == 1:
-            ModLog.Log("ModLogSer-Start\n")
-            ModLogSer.LoginService()
-            ModLog.Log("ModLogSer-End\n")
-
-        if Choix == 2:
-            if Disable_HG == ("true"):
-                ModLog.Log("HubGames-Start\n")
-                ModLog.Log("Transfert de la demande vers le service Hub Games\n")
-                HubGames.HubGames()
-                ModLog.Log("HubGames-End\n")
-            else:
-                print("Ce service à été désactivé par votre administrateur")
-
-        if Choix == 3:
-            if Disable_HS == ("true"):
-                ModLog.Log("HubServer-Start\n")
-                ModLog.Log("Transfert de la demande vers le service Hub Server\n")
-                HubServer.HubServer()
-                ModLog.Log("HubServer-End\n")
-            else:
-                print("Ce service à été désactivé par votre Administrateur")
-        
-        if Choix == 4:
-            os.system(Term_clear)
-            ModLog.Log("Services desactivation")
-            print("Interface de désactivation des services")
-            print("")
-            print("1. HubGames")
-            print("2. HubServer")
-            
-            Choix = int(input("Entrez le service à désactiver : "))
-            
+            print("Tableau Administrateur")
+    
+            print("Menu des services")
+            print()
+            print("1. Login Service.")
+            print("2. Jeux.")
+            print("3. Serveurs.")
+            print("4. Désactivation des services.")
+            print("5. Autres Apps")
+            print("6. Nouveautés")
+            print("7. Exit")
+    
+    
+            Choix = int(input("Entrez le service à appeler : "))
+    
             if Choix == 1:
-                ModLog.Log("HubGames-Disable")
-                if Disable_HG == ("true"):
-                    ModLog.Log("Désactivation")
-                    Disable_HG = ("false")
-                if Disable_HG == ("false"):
-                    ModLog.Log("Activation")
-                    Disable_HG = ("true")
-            
+                ModLog.Log("ModLogSer-Start\n")
+                ModLogSer.LoginService()
+                ModLog.Log("ModLogSer-End\n")
+    
             if Choix == 2:
-                ModLog.Log("HubServer-Disable")
+                if Disable_HG == ("true"):
+                    ModLog.Log("HubGames-Start\n")
+                    ModLog.Log("Transfert de la demande vers le service Hub Games\n")
+                    HubGames.HubGames()
+                    ModLog.Log("HubGames-End\n")
+                else:
+                    print("Ce service à été désactivé par votre administrateur")
+    
+            if Choix == 3:
                 if Disable_HS == ("true"):
-                    ModLog.Log("Désactivation")
-                    Disable_HS = ("false")
-                if Disable_HS == ("false"):
-                    ModLog.Log("Activation")
-                    Disable_HS = ("true")
+                    ModLog.Log("HubServer-Start\n")
+                    ModLog.Log("Transfert de la demande vers le service Hub Server\n")
+                    HubServer.HubServer()
+                    ModLog.Log("HubServer-End\n")
+                else:
+                    print("Ce service à été désactivé par votre Administrateur")
             
-        if Choix == 5:
-            print("Autres Apps")
-            ModLog.Log("Autres Apps")
-            ModApps.Others_apps()
-        
-        if Choix == 6:
-            ModLog.Log("ModNews\n")
-            ModNews.News()
+            if Choix == 4:
+                os.system(Term_clear)
+                ModLog.Log("Services desactivation")
+                print("Interface de désactivation des services")
+                print("")
+                print("1. HubGames")
+                print("2. HubServer")
+                
+                Choix = int(input("Entrez le service à désactiver : "))
+                
+                if Choix == 1:
+                    ModLog.Log("HubGames-Disable")
+                    if Disable_HG == ("true"):
+                        ModLog.Log("Désactivation")
+                        Disable_HG = ("false")
+                    if Disable_HG == ("false"):
+                        ModLog.Log("Activation")
+                        Disable_HG = ("true")
+                
+                if Choix == 2:
+                    ModLog.Log("HubServer-Disable")
+                    if Disable_HS == ("true"):
+                        ModLog.Log("Désactivation")
+                        Disable_HS = ("false")
+                    if Disable_HS == ("false"):
+                        ModLog.Log("Activation")
+                        Disable_HS = ("true")
+                
+            if Choix == 5:
+                print("Autres Apps")
+                ModLog.Log("Autres Apps")
+                ModApps.Others_apps()
             
-        
-        if Choix == 7:
-            ModLog.Log("Exit\n")
-            input("Appuyez pour fermer...")
-            ModLog.Log("End\n")
-            ModLog.Log("\n")
-            ModLog.Log("\n")
-            ModLog.Log("\n")
-            os.system("pkill -f 'terminal'")
-            exit("reportez les logs")
+            if Choix == 6:
+                ModLog.Log("ModNews\n")
+                ModNews.News()
+                
+            
+            if Choix == 7:
+                ModLog.Log("Exit\n")
+                input("Appuyez pour fermer...")
+                ModLog.Log("End\n")
+                ModLog.Log("\n")
+                ModLog.Log("\n")
+                ModLog.Log("\n")
+                os.system("pkill -f 'terminal'")
+                exit("reportez les logs")
+    
+    if Status == ("Basique"):
+        print("Bienvenue sur votre Environnement Numérique (EN) ! ")
